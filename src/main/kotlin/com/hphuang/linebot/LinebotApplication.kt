78845035b788d45fb48da30722409da07f4d39d6
hp.huang <stitchmos@hotmail.com>
2018-06-30 23:13:15 +0800

    handle text message event

--- src/main/kotlin/com/hphuang/linebot/LinebotApplication.kt
+++ src/main/kotlin/com/hphuang/linebot/LinebotApplication.kt
@@ -1,10 +1,29 @@
 package com.hphuang.linebot
 
+import com.linecorp.bot.model.event.Event
+import com.linecorp.bot.model.event.MessageEvent
+import com.linecorp.bot.model.event.message.TextMessageContent
+import com.linecorp.bot.model.message.TextMessage
+import com.linecorp.bot.spring.boot.annotation.EventMapping
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class LinebotApplication
+@LineMessageHandler
+class LinebotApplication {
+
+    @EventMapping
+    fun handleTextMessageEvent (messageEvent: MessageEvent<TextMessageContent>): TextMessage {
+        val text = messageEvent.message.text
+        return TextMessage(text)
+    }
+
+    @EventMapping
+    fun handleDefaultMessageEvent(event: Event) {
+        println("handleDefaultMessageEvent")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<LinebotApplication>(*args)
