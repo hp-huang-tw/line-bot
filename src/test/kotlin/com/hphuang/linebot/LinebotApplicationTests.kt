d33e0d336df9df3a429910c6fe50ccccbbfe1447
hp.huang <stitchmos@hotmail.com>
2018-06-30 22:58:43 +0800

    initial commit

---
+++ src/test/kotlin/com/hphuang/linebot/LinebotApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.hphuang.linebot
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class LinebotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
